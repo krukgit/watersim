811469fb738642ba0a087a37a65a3f379f627e96
krukgit <maciej.kruk@gmail.com>
2013-01-25 00:44:29 +0100

    working pool texture

--- pool.fs
+++ pool.fs
@@ -8,5 +8,13 @@ uniform sampler2D tex;
 void main()
 {
     //outColor = vec4( 1.0, height+1., 0.0, 1.0 );
-    outColor = texture(tex, texCoord.xy);
+    //outColor = vec4(texCoord.x, texCoord.y, texCoord.z, 1.0);
+    //outColor = texture(tex, texCoord.xy);
+    if (texCoord.x < -0.9999f || texCoord.x > 0.9999f)
+        outColor = texture(tex, texCoord.yz);
+    else if (texCoord.y < -0.9999f || texCoord.y > 0.9999f)
+        outColor = texture(tex, texCoord.xz);
+    else 
+        outColor = texture(tex, texCoord.xy);
+
 }
--- pool.vs
+++ pool.vs
@@ -12,6 +12,14 @@ void main()
 {
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
 	height = position.y;
-	texCoord = position;
+	texCoord = position.xyz;
+	/*if (texCoord.x == 0.0f || texCoord.x == 1.0f)
+		texCoord = position.yz*0.5f+0.5f;
+	else if (texCoord.y == 0.0f || texCoord.y == 1.0f)
+		texCoord = position.xz*0.5f+0.5f;
+	else
+		texCoord = position.xy*0.5f+0.5f;
+		*/
+	//;texCoord = position;
 }
 
